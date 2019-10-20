32b7b653df2da1ce1f53279cfed7d36a8fc68a56
Rob Fazio <reemfz@gmail.com>
2019-10-20 17:18:06 -0500

    refactored the home view into sub views

--- DesignCode/Home.swift
+++ DesignCode/Home.swift
@@ -9,26 +9,30 @@
 import SwiftUI
 
 struct Home: View {
-    var menu = menuData
+    
+    @State private var show = false
+    @State private var showProfile = false
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
+        
+        ZStack {
+            ContentView()
+                .background(Color.white)
+                .cornerRadius(30)
+                .shadow(radius: 20)
+                .animation(.spring())
+                .offset(y: showProfile ? 40 : UIScreen.main.bounds.height)
             
-            ForEach(menu) { item in
-                MenuRowView(image: item.icon, text: item.title)
-            }
+            MenuButton(show: $show)
+                .offset(x: -30, y: showProfile ? 0 : 80)
+                .animation(.spring())
             
-            Spacer()
+            MenuRight(show: $showProfile)
+                .offset(x: -16, y: showProfile ? 0 : 88)
+                .animation(.spring())
+            
+            MenuView(show: $show)
         }
-        .padding(.top, 20)
-        .padding(30)
-        .frame(minWidth: 0, maxWidth: .infinity)
-        .background(Color.white)
-        .cornerRadius(30)
-        .padding(.trailing, 60)
-        .shadow(radius: 20)
-        
-        
     }
 }
 
@@ -68,3 +72,103 @@ let menuData = [
     Menu(title: "Team", icon: "person.2"),
     Menu(title: "Sign Out", icon: "arrow.uturn.down")
 ]
+
+struct MenuView: View {
+    
+    var menu = menuData
+    @Binding var show: Bool
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            
+            ForEach(menu) { item in
+                MenuRowView(image: item.icon, text: item.title)
+            }
+            
+            Spacer()
+        }
+        .padding(30)
+        .frame(minWidth: 0, maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+        .padding(.trailing, 60)
+        .shadow(radius: 20)
+        .rotation3DEffect(Angle(degrees: show ? 0 : 60), axis: (x: 0, y: 10.0, z: 0))
+        .animation(.default)
+        .offset(x: show ? 0 : -UIScreen.main.bounds.width, y: 0)
+        .onTapGesture {
+            self.show.toggle()
+        }
+    }
+}
+
+struct CircleButton: View {
+    
+    var icon = ""
+    
+    var body: some View {
+        HStack {
+            Image(systemName: icon)
+                .foregroundColor(Color.black)
+        }
+        .frame(width: 44, height: 44)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(color: Color("buttonShadow"), radius: 10, x: 0, y: 10)
+    }
+}
+
+struct MenuButton: View {
+    
+    @Binding var show: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button(action: {
+                    self.show.toggle()
+                }) {
+                    HStack {
+                        Spacer()
+                        Image(systemName: "list.dash")
+                            .foregroundColor(Color.black)
+                    }
+                    .padding(.trailing, 20)
+                    .frame(width: 90, height: 60)
+                    .background(Color.white)
+                    .cornerRadius(30)
+                    .shadow(color: Color("buttonShadow"), radius: 10, x: 0, y: 10)
+                }
+                Spacer()
+            }
+            Spacer()
+        }
+    }
+}
+
+struct MenuRight: View {
+    
+    @Binding var show: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                HStack {
+                    Button(action: {
+                        self.show.toggle()
+                    }) {
+                        CircleButton(icon: "person.crop.circle")
+                    }
+                    Button(action: {
+                        self.show.toggle()
+                    }) {
+                        CircleButton(icon: "bell")
+                    }
+                }
+                
+            }
+            Spacer()
+        }
+    }
+}
