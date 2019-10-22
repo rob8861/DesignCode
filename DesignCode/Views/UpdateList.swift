edf744c678fc7d9499793c4144cc0d3037e6ec3c
Rob Fazio <reemfz@gmail.com>
2019-10-21 23:54:37 -0500

    added the store as an observedObject. List refactored. added delete and edit functionality

--- DesignCode/Views/UpdateList.swift
+++ DesignCode/Views/UpdateList.swift
@@ -10,49 +10,62 @@ import SwiftUI
 
 struct UpdateList: View {
     
-    @State private var isPresented = false
-    
-    var updates = updateData
+    @ObservedObject var store = UpdateStore(updates: updateData)
     
     var body: some View {
         NavigationView {
-            List(updates) { item in
-                NavigationLink(destination: UpdateDetails(title: item.title, text: item.text, image: item.image)) {
-                    HStack(spacing: 12.0) {
-                        Image(item.image)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 80, height: 80)
-                            .background(Color("background"))
-                            .cornerRadius(20)
-                        VStack(alignment: .leading) {
-                            Text(item.title)
-                                .font(.headline)
-                            Text(item.text)
-                                .lineLimit(2)
-                                .lineSpacing(4)
-                                .font(.subheadline)
-                                .frame(height: 50.0)
-                            Text(item.date)
-                                .font(.caption)
-                                .fontWeight(.bold)
-                                .foregroundColor(.gray)
+            List {
+                ForEach(store.updates) { item in
+                    NavigationLink(destination: UpdateDetails(title: item.title, text: item.text, image: item.image)) {
+                        HStack(spacing: 12.0) {
+                            Image(item.image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 80, height: 80)
+                                .background(Color("background"))
+                                .cornerRadius(20)
+                            VStack(alignment: .leading) {
+                                Text(item.title)
+                                    .font(.headline)
+                                Text(item.text)
+                                    .lineLimit(2)
+                                    .lineSpacing(4)
+                                    .font(.subheadline)
+                                    .frame(height: 50.0)
+                                Text(item.date)
+                                    .font(.caption)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.gray)
+                            }
                         }
                     }
+                    .padding(.vertical, 8.0)
                 }
-                .padding(.vertical, 8.0)
+                .onDelete { (index) in
+                    self.store.updates.remove(at: index.first!)
+                }
+                .onMove(perform: move)
             }
             .navigationBarTitle(Text("Updates"))
-            .navigationBarItems(trailing:
-                Button(action: {
-                    self.isPresented.toggle()
-                }) {
-                    Image(systemName: "gear")
+            .navigationBarItems(leading:
+                Button(action: addUpdate) {
+                    Text("Add Update")
+                        .foregroundColor(.white)
                 }
-            ).sheet(isPresented: $isPresented) {
-                Text("Settings")
-            }
+                .padding(8)
+                .background(Color("background3"))
+                .cornerRadius(8)
+                , trailing: EditButton()
+            )
         }
+    }
+    
+    func addUpdate() {
+        store.updates.append(Update(image: "Certificate1", title: "New Title", text: "New Text", date: "JUL 1"))
+    }
+    
+    func move(from source: IndexSet, to destination: Int) {
+        store.updates.move(fromOffsets: source, toOffset: destination)
     }
 }
 
@@ -61,15 +74,3 @@ struct UpdateList_Previews: PreviewProvider {
         UpdateList()
     }
 }
-
-// date model
-
-let updateData = [
-    Update(image: "Illustration1", title: "SwiftUI", text: "Learn how to build custom views and controls in SwiftUI with advanced composition, layout, graphics, and animation. See a demo of a high performance, animatable control and watch it made step by step in code. Gain a deeper understanding of the layout system of SwiftUI.", date: "JUN 26"),
-    Update(image: "Illustration2", title: "Framer X", text: "Learn how to build custom views and controls in SwiftUI with advanced composition, layout, graphics, and animation. See a demo of a high performance, animatable control and watch it made step by step in code. Gain a deeper understanding of the layout system of SwiftUI.", date: "JUN 11"),
-    Update(image: "Illustration3", title: "CSS Layout", text: "Learn how to combine CSS Grid, Flexbox, animations and responsive design to create a beautiful prototype in CodePen.", date: "MAY 26"),
-    Update(image: "Illustration4", title: "React Native Part 2", text: "Learn how to implement gestures, Lottie animations and Firebase login.", date: "MAY 15"),
-    Update(image: "Certificate1", title: "Unity", text: "Unity course teaching basics, C#, assets, level design and gameplay", date: "MAR 19"),
-    Update(image: "Certificate2", title: "React Native for Designers", text: "Build your own iOS and Android app with custom animations, Redux and API data.", date: "FEB 14"),
-    Update(image: "Certificate3", title: "Vue.js", text: "Make a dashboard web-app with a complete login system, dark mode, and animated charts for your data.", date: "JAN 23")
-]
